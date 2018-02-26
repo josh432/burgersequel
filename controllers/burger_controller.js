d1fad99a07897e8727579d360da20fbd64be8603
Josh Silverman <jrsilverman71@hotmail.com>
2018-02-26 12:45:42 -0800

    burger insert into DB

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -13,21 +13,22 @@ router.get('/burgers', function (req, res) {
   });
 });
 
-// router.post('/burgers', function(req, res) {
-//   burger.insertOne(['burger_name'], [req.body.burger_name], function(data) {
-//     res.redirect('/');
-//   });
-// });
 
-// router.put('/burgers/:id', function(req, res){
-// 	var condition = "id = " + req.params.id;
-// 	  burger.updateOne({
-//       devoured: true
-//    }, condition, function(data) {
-//     res.redirect('/');
-//   });
+router.post('/burgers', function(req, res) {
+  db.Burger.create({burger_name: req.body.burger_name}, function(data) {
+    res.redirect('/');
+  });
+});
 
-// });
+router.put('/burgers/:id', function(req, res){
+	var condition = "id = " + req.params.id;
+	  db.Burger.update({
+      devoured: true
+   }, condition, function(data) {
+    res.redirect('/');
+  });
+
+});
 
 
 
